95d74357c1a70b357bf2a4daee01c9ff674c5e3a
Pebhashi Tharushika <pebhashitharushika@gmail.com>
2025-07-06 14:32:03 +0530

    add doc comments

--- src/main/java/com/mbpt/nexbank/auth/repository/UserRepository.java
+++ src/main/java/com/mbpt/nexbank/auth/repository/UserRepository.java
@@ -9,9 +9,22 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
+    /**
+     * Find user by username
+     *
+     * @param username username
+     * @return user entity
+     */
     @Query(value = "select * from user where username=:username", nativeQuery = true)
     public UserEntity findUserByUserName(@Param("username") String username);
 
+
+    /**
+     * Find scopes
+     *
+     * @param userId user id
+     * @return list of scopes
+     */
     @Query(value = "select permission_id, permission from permission p inner join role_permission rp on p.id = rp.permission_id inner join role r on rp.role_id = r.id where role = 'admin';", nativeQuery = true)
     public String findScopes(int userId);
 }
